f0b56dcebca6b3d77c18429f35f325302914a7a4
RamKishor <ramkishor142000@gmail.com>
2026-01-03 03:27:16 +0530

    not image upload without login or remove if not blog created

--- src/components/blog/create-blog/create-blog.jsx
+++ src/components/blog/create-blog/create-blog.jsx
@@ -1,18 +1,21 @@
-import React, {useEffect, useState, useRef} from 'react'
-import {Editor} from '@tinymce/tinymce-react';
-import {Flex, Image, Input, Button, Card} from 'antd';
+import React, { useEffect, useState, useRef } from 'react'
+import { Editor } from '@tinymce/tinymce-react';
+import { Flex, Image, Input, Button, Card, message } from 'antd';
 import envObj from '../../../environmentConfig'
 import bucket from '../../../appwrite/bucket';
 import blog from '../../../appwrite/blog';
-import {useSelector} from 'react-redux';
-import {useLocation, useNavigate} from 'react-router';
-import {UploadOutlined} from '@ant-design/icons';
+import { useSelector } from 'react-redux';
+import { useLocation, useNavigate } from 'react-router';
+import { UploadOutlined } from '@ant-design/icons';
 
 
-const CreateBlog = ({name, control, label, defaultValue = ""}) => {
+
+const CreateBlog = ({ name, control, label, defaultValue = "" }) => {
     const [uploadImageDetail, setUploadImageDetail] = useState(null);
     const [description, setDescription] = useState("");
     const [title, setTitle] = useState("");
+    const [messageApi, contextHolder] = message.useMessage();
+
     const loginUserId = useSelector((store) => store.authState)
     const imgId = useRef(null)
     const editorRef = useRef(null);
@@ -21,26 +24,29 @@ const CreateBlog = ({name, control, label, defaultValue = ""}) => {
     console.log("location", location)
 
     useEffect(() => {
-        console.log("loginUserId", loginUserId)
-        if(location&& location.state&&location.state.data.$id){
+        console.log("loginUserId", loginUserId);
+        if (location && location.state && location.state.data.$id) {
             imgId.current = location.state.data.featuredImage;
             setTitle(location.state.data.title)
             setDescription(location.state.data.content)
             setUploadImageDetail(location.state.data.featuredImage)
         }
-        return () => {
-            // console.log(uploadImageDetail.$id, title, loginUserId)
-            if (imgId.current && imgId.current.$id) {
-                // delete
-                console.log(uploadImageDetail);
-                bucket.deleteImage(imgId.current.$id)
-                    .then(result => console.log(result))
-                    .catch(e => console.log(e))
+
+        return async () => {
+            try {
+                console.log(uploadImageDetail.$id, title, loginUserId)
+                if (imgId.current && imgId.current.$id) {
+                    console.log(uploadImageDetail);
+                    const result = await bucket.deleteImage(imgId.current.$id)
+                    console.log(result)
+                }
+            } catch (error) {
+                messageApi.info(error.message);
             }
         }
     }, [])
     const log = async () => {
-        if (uploadImageDetail.$id) {
+        if (uploadImageDetail?.$id) {
             if (editorRef.current) {
                 console.log(editorRef.current.getContent());
             }
@@ -62,6 +68,7 @@ const CreateBlog = ({name, control, label, defaultValue = ""}) => {
             }
 
         } else {
+            contextHolder("Please first add image then submit post")
             console.log("please first add image then submit post")
         }
 
@@ -74,9 +81,15 @@ const CreateBlog = ({name, control, label, defaultValue = ""}) => {
         // bucket.addImage()
     }
     const uploadFile = async (e) => {
-        console.log(e.target.files[0]);
-        const file = e.target.files[0]
         try {
+            if (!loginUserId?.data?.$id) {
+                throw new Error("Please login first to upload Image!");
+            }
+
+            const file = e.target.files[0];
+            if (!file) return;
+
+            // your upload logic
             console.log(uploadImageDetail)
             if (uploadImageDetail !== null) {
                 // update
@@ -90,99 +103,102 @@ const CreateBlog = ({name, control, label, defaultValue = ""}) => {
                 setUploadImageDetail(image);
                 // toast.success("success upload");
             }
-
         } catch (error) {
-            console.log(error)
+            console.error(error.message);
+            // graceful UI handling
+            messageApi.info(error.message);
         }
-    }
+    };
+
 
     return (
         <Card>
-          <Flex>
-            <div>
-              <Card>
-                <label name={"image"} htmlFor='image'>Upload Image
-                  <Input id='image' name='image' onChange={(e) => uploadFile(e)} type="file"
-                         title={<UploadOutlined/>}/>
-                </label>
-                <label htmlFor='title'>Title
-                  <Input id='title' name='title'  value={title??""} placeholder='Enter Title' type='text'
-                         onChange={(e) => setTitle(e.target.value)}/>
-                </label>
-              </Card>
+            <Flex>
+                {contextHolder}
+                <div>
+                    <Card>
+                        <label name={"image"} htmlFor='image'>Upload Image
+                            <Input id='image' name='image' onChange={(e) => uploadFile(e)} type="file"
+                                title={<UploadOutlined />} />
+                        </label>
+                        <label htmlFor='title'>Title
+                            <Input id='title' name='title' value={title ?? ""} placeholder='Enter Title' type='text'
+                                onChange={(e) => setTitle(e.target.value)} />
+                        </label>
+                    </Card>
 
-              {/*{uploadImageDetail && <Card>*/}
-              {/*  <Image*/}
-              {/*      width={600}*/}
-              {/*      height={270}*/}
-              {/*      src={envObj.bucketImageBaseUrl.replace("imageId", uploadImageDetail.$id)}*/}
-              {/*      alt='...'*/}
-              {/*  />*/}
-              {/*</Card>}*/}
-              {imgId.current && <Card>
-                <Image
-                    width={600}
-                    height={270}
-                    src={envObj.bucketImageBaseUrl.replace("imageId", imgId.current)}
-                    alt='...'
-                />
-              </Card>}
-            </div>
+                    {/*{uploadImageDetail && <Card>*/}
+                    {/*  <Image*/}
+                    {/*      width={600}*/}
+                    {/*      height={270}*/}
+                    {/*      src={envObj.bucketImageBaseUrl.replace("imageId", uploadImageDetail.$id)}*/}
+                    {/*      alt='...'*/}
+                    {/*  />*/}
+                    {/*</Card>}*/}
+                    {imgId.current && <Card>
+                        <Image
+                            width={600}
+                            height={270}
+                            src={envObj.bucketImageBaseUrl.replace("imageId", imgId.current)}
+                            alt='...'
+                        />
+                    </Card>}
+                </div>
 
-            <Card>
-              <Editor
-                  apiKey={envObj.tinymceKey}
-                  init={{
-                    // plugins: [
-                    //   // Core editing features
-                    //   'anchor', 'autolink', 'charmap', 'codesample', 'emoticons', 'image', 'link', 'lists', 'media', 'searchreplace', 'table', 'visualblocks', 'wordcount',
-                    //   // Your account includes a free trial of TinyMCE premium features
-                    //   // Try the most popular premium features until Dec 14, 2024:
-                    //   'checklist', 'mediaembed', 'casechange', 'export', 'formatpainter', 'pageembed', 'a11ychecker', 'tinymcespellchecker', 'permanentpen', 'powerpaste', 'advtable', 'advcode', 'editimage', 'advtemplate', 'ai', 'mentions', 'tinycomments', 'tableofcontents', 'footnotes', 'mergetags', 'autocorrect', 'typography', 'inlinecss', 'markdown',
-                    //   // Early access to document converters
-                    //   'importword', 'exportword', 'exportpdf'
-                    // ],
-                    plugins: [
-                      "image",
-                      "advlist",
-                      "autolink",
-                      "lists",
-                      "link",
-                      "image",
-                      "charmap",
-                      "preview",
-                      "anchor",
-                      "searchreplace",
-                      "visualblocks",
-                      "code",
-                      "fullscreen",
-                      "insertdatetime",
-                      "media",
-                      "table",
-                      "code",
-                      "help",
-                      "wordcount",
-                      "anchor",
-                    ],
-                    // toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck typography | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat',
-                    // tinycomments_mode: 'embedded',
-                    // tinycomments_author: 'Author name',
-                    // mergetags_list: [
-                    //   { value: 'First.Name', title: 'First Name' },
-                    //   { value: 'Email', title: 'Email' },
-                    // ],
-                    // ai_request: (request, respondWith) => respondWith.string(() => Promise.reject('See docs to implement AI Assistant')),
-                    toolbar:
-                        "undo redo | blocks | image | bold italic forecolor | alignleft aligncenter bold italic forecolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent |removeformat | help",
-                    content_style: "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }"
-                  }}
-                  initialValue={description??""}
-                  // style={{maxWidth: 600}}
-                  onFocusOut={onChange}
-              />
-              <Button onClick={log}>Upload Blog</Button>
-            </Card>
-          </Flex>
+                <Card>
+                    <Editor
+                        apiKey={envObj.tinymceKey}
+                        init={{
+                            // plugins: [
+                            //   // Core editing features
+                            //   'anchor', 'autolink', 'charmap', 'codesample', 'emoticons', 'image', 'link', 'lists', 'media', 'searchreplace', 'table', 'visualblocks', 'wordcount',
+                            //   // Your account includes a free trial of TinyMCE premium features
+                            //   // Try the most popular premium features until Dec 14, 2024:
+                            //   'checklist', 'mediaembed', 'casechange', 'export', 'formatpainter', 'pageembed', 'a11ychecker', 'tinymcespellchecker', 'permanentpen', 'powerpaste', 'advtable', 'advcode', 'editimage', 'advtemplate', 'ai', 'mentions', 'tinycomments', 'tableofcontents', 'footnotes', 'mergetags', 'autocorrect', 'typography', 'inlinecss', 'markdown',
+                            //   // Early access to document converters
+                            //   'importword', 'exportword', 'exportpdf'
+                            // ],
+                            plugins: [
+                                "image",
+                                "advlist",
+                                "autolink",
+                                "lists",
+                                "link",
+                                "image",
+                                "charmap",
+                                "preview",
+                                "anchor",
+                                "searchreplace",
+                                "visualblocks",
+                                "code",
+                                "fullscreen",
+                                "insertdatetime",
+                                "media",
+                                "table",
+                                "code",
+                                "help",
+                                "wordcount",
+                                "anchor",
+                            ],
+                            // toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck typography | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat',
+                            // tinycomments_mode: 'embedded',
+                            // tinycomments_author: 'Author name',
+                            // mergetags_list: [
+                            //   { value: 'First.Name', title: 'First Name' },
+                            //   { value: 'Email', title: 'Email' },
+                            // ],
+                            // ai_request: (request, respondWith) => respondWith.string(() => Promise.reject('See docs to implement AI Assistant')),
+                            toolbar:
+                                "undo redo | blocks | image | bold italic forecolor | alignleft aligncenter bold italic forecolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent |removeformat | help",
+                            content_style: "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }"
+                        }}
+                        initialValue={description ?? ""}
+                        // style={{maxWidth: 600}}
+                        onFocusOut={onChange}
+                    />
+                    <Button disabled={uploadImageDetail?.$id ? false : true} onClick={log}>Upload Blog</Button>
+                </Card>
+            </Flex>
         </Card>
     )
 }
