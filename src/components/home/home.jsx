bc23f7314c9b2ae330d3a5e14bb82de949f98174
Ram Kishor <rkishor@okruti.com>
2026-02-28 00:17:49 +0530

    style blog listing page with shimar ui

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -8,9 +8,11 @@ import database from "../../appwrite/blog";
 import { userStatus } from "../../store/auth-reducer";
 import { Select } from "antd";
 const { Option } = Select;
+import { motion } from "framer-motion";
 
 const Home = () => {
   const [blogsList, setBlogsList] = useState([]);
+  const [loading, setLoading] = useState(true);
   const dispatch = useDispatch();
 
   const userLoginStatus = async () => {
@@ -25,8 +27,10 @@ const Home = () => {
   };
   const fetchData = async () => {
     try {
+      setLoading(true);
       const blogList = await database.listBlog();
       setBlogsList(blogList.documents);
+      setLoading(false);
     } catch (error) {
       console.log(error);
     }
@@ -43,20 +47,7 @@ const Home = () => {
     };
   };
   return (
-    // <div className="header-footer-gap bg-cyan-950">
-    //   <div style={{ display: "flex", justifyContent: "space-between", marginLeft: 10, alignItems: 'center' }}>
-    //     <h1>My Blogs</h1>
-    //     {/*todo apply filter */}
-    //     <Select style={{ width: 150 }} defaultActiveFirstOption={true} defaultValue={'My Blogs'}>
-    //       <Option value="My Blogs">My Blogs</Option>
-    //       <Option value="Others Blogs">Others Blog</Option>
-    //     </Select>
-    //   </div>
-    //   <div style={{ display: "flex", flexWrap: 'wrap', justifyContent: "center", gap: 20 }}>
-    //     {blogsList.map((blog) => <Blog key={blog.$id} {...createProps(blog)} />)}
-    //   </div>
-    // </div>
-    <div className="header-footer-gap bg-cyan-950 min-h-screen px-10">
+    <div className="header-footer-gap bg-slate-950 min-h-screen px-10">
       <div className="flex justify-between items-center">
         <h1 className="text-3xl font-bold text-white">My Blogs</h1>
 
@@ -66,13 +57,59 @@ const Home = () => {
         </Select>
       </div>
 
-      <div className="grid md:grid-cols-3 sm:grid-cols-2 gap-8 justify-items-center mt-10">
-        {blogsList.map((blog) => (
-          <Blog key={blog.$id} {...createProps(blog)} />
-        ))}
+      <div className="grid md:grid-cols-4 sm:grid-cols-2 gap-8 justify-items-center mt-10">
+        {loading ? (
+          <>
+            {/* <div className="col-span-full text-center text-white flex flex-col justify-center"> */}
+            {/* Loading... */}
+            {Array(6)
+              .fill(0)
+              .map((_, index) => (
+                <motion.div
+                  initial={{ opacity: 0, y: 0 }}
+                  whileInView={{ opacity: 1, y: 0 }}
+                  transition={{ duration: 0.6, ease: "easeOut" }}
+                  viewport={{ once: true }}
+                  className="bg-linear-to-br from-slate-900 to-slate-800 
+             border border-slate-700 rounded-2xl 
+             shadow-lg hover:scale-105 transition duration-300 
+             w-87.5 overflow-hidden cursor-pointer"
+                >
+                  <BlogSkeleton key={index} />
+                </motion.div>
+              ))}
+            {/* </div> */}
+          </>
+        ) : (
+          // </div>
+          blogsList.map((blog) => (
+            <Blog key={blog.$id} {...createProps(blog)} />
+          ))
+        )}
       </div>
     </div>
   );
 };
 
 export default Home;
+
+const BlogSkeleton = () => {
+  return (
+    <div
+      className="bg-slate-800 border border-slate-700 
+                    rounded-2xl w-[350px] p-5 animate-pulse"
+    >
+      <div className="h-48 bg-slate-700 rounded-lg mb-4"></div>
+
+      <div className="h-6 bg-slate-700 rounded w-3/4 mb-3"></div>
+
+      <div className="h-4 bg-slate-700 rounded w-full mb-2"></div>
+      <div className="h-4 bg-slate-700 rounded w-5/6 mb-2"></div>
+      <div className="h-4 bg-slate-700 rounded w-2/3"></div>
+
+      <div className="h-8 bg-slate-700 rounded mt-5 w-1/3"></div>
+    </div>
+  );
+};
+
+// export default BlogSkeleton;
