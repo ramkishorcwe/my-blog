f0083364941bab75c6e67dcf90a6503908ee21a8
Ram Kishor <rkishor@okruti.com>
2026-02-27 22:05:08 +0530

    style blog list/home page with blog card

--- src/components/blog/blogs-list/blogs.jsx
+++ src/components/blog/blogs-list/blogs.jsx
@@ -1,112 +1,207 @@
-import { DeleteOutlined, EditOutlined } from '@ant-design/icons'
-import HTMLReactParser from 'html-react-parser/lib/index'
-import envConfig from '../../../environmentConfig'
-import { useSelector } from 'react-redux'
-import blogConfig from '../../../appwrite/blog'
+import { DeleteOutlined, EditOutlined } from "@ant-design/icons";
+import HTMLReactParser from "html-react-parser/lib/index";
+import envConfig from "../../../environmentConfig";
+import { useSelector } from "react-redux";
+import blogConfig from "../../../appwrite/blog";
 import { useNavigate } from "react-router";
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
-import CardActionArea from '@mui/material/CardActionArea';
-import { message } from 'antd'
-import bucket from '../../../appwrite/bucket'
-import '../blog.css'
+import { message } from "antd";
+import bucket from "../../../appwrite/bucket";
+import "../blog.css";
+
+// const BlogCard = (props) => {
+//   const [messageApi, contextHolder] = message.useMessage();
+//   const loginUser = useSelector((store) => {
+//     return store.authState.userData;
+//   });
+//   const imageUrl = envConfig.bucketImageBaseUrl.replace(
+//     "imageId",
+//     props.featuredImage,
+//   );
+//   const navigate = useNavigate();
+
+//   const deleteMyBlog = async (id, imgId) => {
+//     try {
+//       await blogConfig.deleteBlog(id);
+//       if (imgId) {
+//         const result = await bucket.deleteImage(imgId);
+//         console.log(result);
+//       }
+//       messageApi.error("Blog Delete Successful!");
+//       props.fetchData();
+//     } catch (error) {
+//       messageApi.error(error.message);
+//       console.log(error);
+//     }
+//   };
+//   const editMyBlog = (data) => {
+//     console.log(data);
+//     navigate("/create-blog", {
+//       state: {
+//         id: data.$id,
+//       },
+//     });
+//   };
+//   return (
+//     <>
+//       {contextHolder}
+//       <Card
+//         className="plp-card"
+//         key={`${props.$id}${props.title}`}
+//         onClick={() => {
+//           navigate(`blog/${props.$id}`, { state: props });
+//         }}
+//       >
+//         <CardActionArea>
+//           <CardMedia
+//             component="img"
+//             className="plp-image"
+//             image={imageUrl}
+//             alt=""
+//             onError={(e) => {
+//               e.target.src = "/vite.svg"; // add any local placeholder
+//             }}
+//           />
+
+//           <CardContent className="plp-content">
+//             <div className="plp-header">
+//               <div className="plp-title">{props.title}</div>
+
+//               {loginUser?.$id === props.userId && (
+//                 <div className="plp-actions">
+//                   <Button
+//                     size="small"
+//                     onClick={(e) => {
+//                       e.stopPropagation();
+//                       editMyBlog(props);
+//                     }}
+//                   >
+//                     <EditOutlined />
+//                   </Button>
+
+//                   <Button
+//                     size="small"
+//                     color="error"
+//                     onClick={(e) => {
+//                       e.stopPropagation();
+//                       deleteMyBlog(props.$id, props.featuredImage);
+//                     }}
+//                   >
+//                     <DeleteOutlined />
+//                   </Button>
+//                 </div>
+//               )}
+//             </div>
+
+//             <div className="plp-description">
+//               {HTMLReactParser(props.content)}
+//             </div>
+//           </CardContent>
+//         </CardActionArea>
+//       </Card>
+//     </>
+//   );
+// };
+
+// export default BlogCard;
 
 const BlogCard = (props) => {
-    const [messageApi, contextHolder] = message.useMessage();
-    const loginUser = useSelector((store) => {
-        return store.authState.userData
+  const [messageApi, contextHolder] = message.useMessage();
+  const loginUser = useSelector((store) => store.authState.userData);
+  const navigate = useNavigate();
+
+  const imageUrl = envConfig.bucketImageBaseUrl.replace(
+    "imageId",
+    props.featuredImage,
+  );
+
+  const deleteMyBlog = async (id, imgId) => {
+    try {
+      await blogConfig.deleteBlog(id);
+      if (imgId) {
+        await bucket.deleteImage(imgId);
+      }
+      messageApi.success("Blog Deleted Successfully!");
+      props.fetchData();
+    } catch (error) {
+      messageApi.error(error.message);
+    }
+  };
+
+  const editMyBlog = (data) => {
+    navigate("/create-blog", {
+      state: { id: data.$id },
     });
-    const imageUrl = envConfig.bucketImageBaseUrl.replace("imageId", props.featuredImage);
-    const navigate = useNavigate();
+  };
 
-    const deleteMyBlog = async (id, imgId) => {
-        try {
-            await blogConfig.deleteBlog(id)
-            if (imgId) {
-                const result = await bucket.deleteImage(imgId)
-                console.log(result)
-            }
-            messageApi.error("Blog Delete Successful!");
-            props.fetchData()
-        } catch (error) {
-            messageApi.error(error.message);
-            console.log(error)
-        }
-    }
-    const editMyBlog = (data) => {
-        console.log(data)
-        navigate("/create-blog", {
-            state: {
-                id: data.$id
-            }
-        });
-    }
-    return (
-        <>
-            {contextHolder}
-            <Card
-                className="plp-card"
-                key={`${props.$id}${props.title}`}
-                onClick={() => {
-                    navigate(`blog/${props.$id}`, { state: props });
-                }}
+  return (
+    <>
+      {contextHolder}
+
+      <div
+        className="bg-gradient-to-br from-slate-900 to-slate-800 
+                           border border-slate-700 rounded-2xl 
+                           shadow-lg hover:scale-105 transition duration-300 
+                           w-[350px] overflow-hidden cursor-pointer"
+        onClick={() => navigate(`/blog/${props.$id}`, { state: props })}
+      >
+        {/* Image */}
+        <div className="h-48 overflow-hidden">
+          <img
+            src={imageUrl}
+            alt=""
+            className="w-full h-full object-cover"
+            onError={(e) => {
+              e.target.src = "/vite.svg";
+            }}
+          />
+        </div>
+
+        {/* Content */}
+        <div className="p-5">
+          <div className="flex justify-between items-start">
+            <h3 className="text-xl font-semibold text-white">{props.title}</h3>
+
+            {loginUser?.$id === props.userId && (
+              <div className="flex gap-2">
+                <button
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    editMyBlog(props);
+                  }}
+                  className="text-gray-400 hover:text-white"
+                >
+                  <EditOutlined />
+                </button>
+
+                <button
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    deleteMyBlog(props.$id, props.featuredImage);
+                  }}
+                  className="text-red-400 hover:text-red-500"
+                >
+                  <DeleteOutlined />
+                </button>
+              </div>
+            )}
+          </div>
+
+          <div className="text-gray-400 mt-3 line-clamp-3 text-sm">
+            {HTMLReactParser(props.content)}
+          </div>
+
+          <div className="mt-5">
+            <button
+              className="border border-white text-white 
+                                       px-4 py-2 rounded-lg 
+                                       hover:bg-white hover:text-black 
+                                       transition"
             >
-                <CardActionArea>
-                    <CardMedia
-                        component="img"
-                        className="plp-image"
-                        image={imageUrl}
-                        alt=""
-                        onError={(e) => {
-                            e.target.src = "/public/vite.svg"; // add any local placeholder
-                        }}
-                    />
-
-
-                    <CardContent className="plp-content">
-                        <div className="plp-header">
-                            <div className="plp-title">{props.title}</div>
-
-                            {loginUser?.$id === props.userId && (
-                                <div className="plp-actions">
-                                    <Button
-                                        size="small"
-                                        onClick={(e) => {
-                                            e.stopPropagation();
-                                            editMyBlog(props);
-                                        }}
-                                    >
-                                        <EditOutlined />
-                                    </Button>
-
-                                    <Button
-                                        size="small"
-                                        color="error"
-                                        onClick={(e) => {
-                                            e.stopPropagation();
-                                            deleteMyBlog(props.$id, props.featuredImage);
-                                        }}
-                                    >
-                                        <DeleteOutlined />
-                                    </Button>
-                                </div>
-                            )}
-                        </div>
-
-                        <div className="plp-description">
-                            {HTMLReactParser(props.content)}
-                        </div>
-                    </CardContent>
-                </CardActionArea>
-            </Card>
-        </>
-
-    )
-}
-
-export default BlogCard
-
-
-
+              Read More
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,66 +1,78 @@
 import Blog from "../blog/blogs-list/blogs";
-import '../../components/'
+import "../../components/";
 import { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router";
-import authService from '../../appwrite/auth'
-import database from '../../appwrite/blog'
-import { userStatus } from '../../store/auth-reducer'
+import authService from "../../appwrite/auth";
+import database from "../../appwrite/blog";
+import { userStatus } from "../../store/auth-reducer";
 import { Select } from "antd";
 const { Option } = Select;
 
 const Home = () => {
-  // const userAuth = useSelector((store) => store.authState.status);
-  const navigate = useNavigate();
   const [blogsList, setBlogsList] = useState([]);
   const dispatch = useDispatch();
 
   const userLoginStatus = async () => {
     try {
-      const user = await authService.getUser()
+      const user = await authService.getUser();
       if (user) {
         dispatch(userStatus({ userData: user, status: true }));
       }
-
     } catch (error) {
-      console.log(error)
-      // navigate('/login')
+      console.log(error);
     }
-  }
+  };
   const fetchData = async () => {
     try {
       const blogList = await database.listBlog();
       setBlogsList(blogList.documents);
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-  }
+  };
   useEffect(() => {
     userLoginStatus();
-    fetchData()
-  }, [])
+    fetchData();
+  }, []);
   const createProps = (blog) => {
     return {
       ...blog,
       key: blog.title,
-      fetchData
-    }
-  }
+      fetchData,
+    };
+  };
   return (
-    <div className="header-footer-gap bg-cyan-950">
-      <div style={{ display: "flex", justifyContent: "space-between", marginLeft: 10, alignItems: 'center' }}>
-        <h1>My Blogs</h1>
-        {/*todo apply filter */}
-        <Select style={{ width: 150 }} defaultActiveFirstOption={true} defaultValue={'My Blogs'}>
+    // <div className="header-footer-gap bg-cyan-950">
+    //   <div style={{ display: "flex", justifyContent: "space-between", marginLeft: 10, alignItems: 'center' }}>
+    //     <h1>My Blogs</h1>
+    //     {/*todo apply filter */}
+    //     <Select style={{ width: 150 }} defaultActiveFirstOption={true} defaultValue={'My Blogs'}>
+    //       <Option value="My Blogs">My Blogs</Option>
+    //       <Option value="Others Blogs">Others Blog</Option>
+    //     </Select>
+    //   </div>
+    //   <div style={{ display: "flex", flexWrap: 'wrap', justifyContent: "center", gap: 20 }}>
+    //     {blogsList.map((blog) => <Blog key={blog.$id} {...createProps(blog)} />)}
+    //   </div>
+    // </div>
+    <div className="header-footer-gap bg-cyan-950 min-h-screen px-10">
+      <div className="flex justify-between items-center">
+        <h1 className="text-3xl font-bold text-white">My Blogs</h1>
+
+        <Select style={{ width: 150 }} defaultValue={"My Blogs"}>
           <Option value="My Blogs">My Blogs</Option>
           <Option value="Others Blogs">Others Blog</Option>
         </Select>
       </div>
-      <div style={{ display: "flex", flexWrap: 'wrap', justifyContent: "center", gap: 20 }}>
-        {blogsList.map((blog) => <Blog key={blog.$id} {...createProps(blog)} />)}
+
+      <div className="grid md:grid-cols-3 sm:grid-cols-2 gap-8 justify-items-center mt-10">
+        {blogsList.map((blog) => (
+          <Blog key={blog.$id} {...createProps(blog)} />
+        ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
