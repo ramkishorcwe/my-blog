3daa66fa28e313c398d58499a2d46cdeed9ce5d3
RamKishor <ramkishor142000@gmail.com>
2026-04-27 23:58:59 +0530

    spliting

--- vite.config.js
+++ vite.config.js
@@ -1,52 +1,60 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'   // ✅ THIS LINE WAS MISSING
+import path from 'path'
 import tailwindcss from '@tailwindcss/vite'
 import { visualizer } from 'rollup-plugin-visualizer';
 
-
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), visualizer({ open: true })],
+  plugins: [
+    react(),
+    tailwindcss(),
+    visualizer({
+      open: true,
+      gzipSize: true
+    })
+  ],
+
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
     },
   },
+
   build: {
     rollupOptions: {
       output: {
-        manualChunks: {
-          reactVendor: ['react', 'react-dom'],
-          router: ['react-router-dom'],
-          mui: ['@mui/material', '@mui/icons-material'],
-        },
-      },
-    },
-  },
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+
+            // 🔹 React core only
+            if (id.includes('node_modules/react/')) {
+              return 'react-core';
+            }
+
+            // 🔹 React DOM
+            if (id.includes('react-dom')) {
+              return 'react-dom';
+            }
+
+            // 🔹 Router
+            if (id.includes('react-router')) {
+              return 'router';
+            }
+
+            // 🔹 Ant Design
+            if (id.includes('antd')) {
+              return 'antd';
+            }
+
+            // 🔹 MUI
+            if (id.includes('@mui')) {
+              return 'mui';
+            }
+
+            return 'vendor';
+          }
+        }
+      }
+    }
+  }
 })
-
-// export default defineConfig({
-//   build: {
-//     rollupOptions: {
-//       output: {
-//         manualChunks(id) {
-//           if (id.includes('node_modules')) {
-//             if (id.includes('react')) return 'reactVendor';
-//             if (id.includes('react-router')) return 'router';
-//             if (id.includes('@mui')) return 'mui';
-//             return 'vendor';
-//           }
-//         },
-//       },
-//     },
-//   },
-// });
-
-// todo : add visualizer plugin to analyze bundle size and optimize it by code splitting and lazy loading.
-// plugins: [
-//   visualizer({
-//     open: true,
-//     gzipSize: true,
-//   }),
-// ]
