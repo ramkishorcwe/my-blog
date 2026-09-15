5279e9913aa0f76e5ff352b7554974b35a763154
okruti-rkishor <rkishor@okruti.com>
2026-09-15 12:27:46 +0530

    update logo and title of app and favicon

--- src/components/utils/logo.jsx
+++ src/components/utils/logo.jsx
@@ -1,12 +1,23 @@
-import React from 'react'
-import logo from '../../assets/logo-1.png'
+import React from "react";
+import logo from "../../assets/logo-1.png";
+import { FileText, CodeXml } from "lucide-react";
 
-const Logo = ({ src = logo, alt = '...' }) => {
+const Logo = ({ src = logo, alt = "..." }) => {
   return (
-    <div style={{ width: 48, height: 36 }}>
-      <img src={src} alt={alt} style={{ width: '100%', height: '100%' }} />
+    <div
+      style={{ width: 58, height: 54, borderRadius: 100, overflow: "hidden" }}
+    >
+      <img
+        src={"/logo.png"}
+        alt={alt}
+        style={{ width: "100%", height: "100%" }}
+      />
     </div>
-  )
-}
+    // <div className="blog-logo">
+    //   <FileText size={32} />
+    //   <CodeXml size={18} />
+    // </div>
+  );
+};
 
-export default Logo
+export default Logo;
